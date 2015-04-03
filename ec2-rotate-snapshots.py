d8cc7b5c0be6a6fd1ad9300230264e71a40a0507
Cheyenne T. Greatorex <cgreatorex@Lightsaber.local>
2015-04-03 13:15:31 -0400

    first version of the rotator script

---
+++ ec2-rotate-snapshots.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+
+import argparse
+import logging
+import os
+import sys
+import subprocess
+import time
+import boto.utils
+import boto.ec2
+
+
+version = sys.hexversion
+if version < 0x02070000:
+    sys.stderr.write('python 2.7 or higher required\n')
+    sys.exit(-1)
+
+
+logging.basicConfig(format = '%(levelname)s: %(message)s', level = logging.INFO)
+
+
+parser = argparse.ArgumentParser(description = 'Rotate EBS snapshots', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--aws-region', default = 'us-east-1', help = 'AWS region', metavar ='REGION')
+parser.add_argument('--aws-access', required = True, help = 'AWS access key', metavar = 'KEY')
+parser.add_argument('--aws-secret', required = True, help = 'AWS secret key', metavar = 'KEY')
+args = parser.parse_args()
+
+aws_region = args.aws_region
+aws_access = args.aws_access
+aws_secret = args.aws_secret
+
+
+# connect to the AWS API
+try:
+    conn = boto.ec2.connect_to_region(aws_region, aws_access_key_id = aws_access, aws_secret_access_key = aws_secret)
+except:
+    logging.critical('unable to connect to the AWS API')
+    sys.exit(-1)
+
+
+# get list of all snapshots
+snapshots = conn.get_all_snapshots(filters={ "status": "completed", "tag:Instance" : "git-000" })
+
+
+print snapshots
+
